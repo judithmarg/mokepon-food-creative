72a4a00d247a3cdd345b07dbf6f9f44e0f3d80ec
judithmarg <judithmargaritapacollalli@gmail.com>
2023-08-16 22:42:23 -0400

    remove alerts

--- js/mokepon.js
+++ js/mokepon.js
@@ -9,13 +9,10 @@ function seleccionarComidaPlayer(){
     let inputFrench = document.getElementById('frenchpapas')
     let spanComidaPlayer = document.getElementById('comida-player')
     if(inputPizza.checked){
-        alert('Pizzate seleccionado')
         spanComidaPlayer.innerHTML = 'Pizzate'
     }else if(inputHambur.checked){
-        alert('Hambur seleccionado')
         spanComidaPlayer.innerHTML = 'Hamburgui'
     }else if(inputFrench.checked){
-        alert('French izzate seleccionado')
         spanComidaPlayer.innerHTML = 'FrenchPapas'
     }else{
         alert('Selecciona una comida por favor')
